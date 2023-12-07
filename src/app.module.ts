d0c755868630089dab95e37a43e196ac7b6b975b
Jeison Rojas <j_esteban96@hotmail.com>
2023-12-06 23:18:43 -0500

    Variables de entor y ambientes creados, listos para ser usados en la app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { environments } from 'environments';
 
 @Module({
-  imports: [ProductsModule, UsersModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      isGlobal: true
+    }),
+    ProductsModule,
+    UsersModule,
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
