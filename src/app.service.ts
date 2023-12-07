1960707f3cd79f989618131aea48adf82e96cc09
Jeison Rojas <j_esteban96@hotmail.com>
2023-12-06 23:43:11 -0500

    Variables de entorno utilizadas sin tipar por medio del método configService.get()

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
 
-  constructor(@Inject('varToUseGlobally') private readonly varToUseGlobally: string) { }
+  constructor(
+    @Inject('varToUseGlobally') private readonly varToUseGlobally: string,
+    private readonly configService: ConfigService
+  ) { }
 
   getHello(): string {
-    return `Hello World! ${this.varToUseGlobally}`;
+    return `Hello World! ${this.varToUseGlobally} in DATABASE_NAME ${this.configService.get('DATABASE_NAME')}`;
   }
 }
