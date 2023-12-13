40b64c4764429e24c08bf39ebf32af0808dfe651
Jeison Rojas <j_esteban96@hotmail.com>
2023-12-13 16:51:33 -0500

    Crear entidad Product con typeorm

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ProductsController } from './controllers/products/products.controller';
 import { ProductsService } from './services/products/products.service';
+import { Product } from './entities/product.entity';
 import { BrandsController } from './controllers/brands/brands.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
 import { BrandsService } from './services/brands/brands.service';
 import { CategoriesService } from './services/categories/categories.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   exports: [ProductsService],
   controllers: [ProductsController, BrandsController, CategoriesController],
   providers: [ProductsService, BrandsService, CategoriesService]
